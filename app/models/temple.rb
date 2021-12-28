13d508a15656e2a5860bab9f6d750b6318e9b968
kajalwebkorps <kajal@webkorps.com>
2021-12-28 13:00:52 +0530

    Remove temple id from user table

--- app/models/temple.rb
+++ app/models/temple.rb
@@ -1,7 +1,6 @@
 class Temple < ApplicationRecord
   has_many :temple_history_details, dependent: :destroy
   has_many :devoters
-  has_many :users
   has_many :offline_city_centres, dependent: :destroy
   has_many :worships, dependent: :destroy
   has_many_attached :temple_images
