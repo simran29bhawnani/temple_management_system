99cc9738b224709cbd7d381fec863fbd4c2197af
kajalwebkorps <kajal@webkorps.com>
2021-12-29 19:49:23 +0530

    Pass temple image url

--- app/models/temple.rb
+++ app/models/temple.rb
@@ -5,4 +5,27 @@ class Temple < ApplicationRecord
   has_many :worships, dependent: :destroy
   has_one_attached :temple_image
   belongs_to :user
+
+  def to_hash
+    {
+      id: self.id,
+      temple_name: self.temple_name,
+      description: self.description,
+      temple_email: self.temple_email,
+      phone_no: self.phone_no,
+      temple_address: self.temple_address,
+      city: self.city,
+      state: self.state,
+      country: self.country,
+      zipcode: self.zipcode,
+      start_time: self.start_time,
+      end_time: self.end_time,
+      user_id: self.user_id,
+      logo: self.temple_logo
+    }
+  end
+
+  def temple_logo
+    self&.temple_image&.service_url if self.temple_image&.attached?
+  end
 end
