d99b3a15e15ea1a6cae8b91da6acdc6ec1b16b68
kajalwebkorps <kajal@webkorps.com>
2021-12-29 15:06:54 +0530

    Remove temple image column from temple table and add cloudinary gem

--- app/models/temple.rb
+++ app/models/temple.rb
@@ -1,9 +1,8 @@
 class Temple < ApplicationRecord
-  include ImageUploader::Attachment(:image)
   has_many :temple_history_details, dependent: :destroy
   has_many :devoters
   has_many :offline_city_centres, dependent: :destroy
   has_many :worships, dependent: :destroy
-  has_many_attached :temple_images
+  has_one_attached :temple_image
   belongs_to :user
 end
